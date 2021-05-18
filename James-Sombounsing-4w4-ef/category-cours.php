3f693a0633b94fda6d4e1a5340b32324a5d9f4a7
Jisei292 <73396433+Jisei292@users.noreply.github.com>
2021-05-18 09:36:51 -0400

    cours content

--- James-Sombounsing-4w4-ef/category-cours.php
+++ James-Sombounsing-4w4-ef/category-cours.php
@@ -28,36 +28,40 @@ get_header();
 			<section class="list-cours">
 			<?php
 			/* Start the Loop */
-            $precedent = "XXXXXXX";
+            $precedent = "XXXXXX";
 			while ( have_posts() ) :
 				the_post();
+
                 $titre_grand = get_the_title();
-				//$sigle = substr($titre_grand,0,7)
-				//$nbHeure = substr($titre_grand,-4,3)
-				//$titrePartiel = substr($titre_grand,8,-6)
+				$sigle = substr($titre_grand,0,7);
+				$nbHeure = substr($titre_grand,-4,3);
+				$titrePartiel = substr($titre_grand,8,-6);
 				$titre = substr($titre_grand,8,-6);
                 $session = substr($titre_grand,4,1);
 				$contenu = get_the_content();
                 $resume = substr($contenu, 0, 200);
 				$typeCours = get_field('type_de_cours');
-				if ($precedent != $typeCours):?>
-				<?php if ($precedent != "XXXXXXX"):?>
-				</section>
+
+				if ($typeCours != $precedent):
+					if ("XXXXXX" != $precedent):?>
+					</section>
+					<?php endif; ?>
+					<section>
 				<?php endif; ?>
-				<section class="list-cours">
-				<?php endif; ?>
+
 			<article>
-			<p> <?php echo $session . " - " . $titre . " - " . $typeCours;  ?> </p>
-			<a href="<?php echo get_permalink() ?>"><?php echo $titre; ?> </a>
-			<p> Session : <?php echo $session ?> </p>
+				<p> <?php echo $typeCours;  ?> </p>
+				<a href="<?php echo get_permalink() ?>"><?php echo $sigle; ?> </a>
+				<p><?php echo $nbHeure ?> </p>
 			
 			</article>
 
             <?php
 			$precedent = $typeCours;
 			endwhile;?>
-			</section>
-			<?php endif; ?>
+			
+		</section>
+		<?php endif; ?>
 		
 
 	</main><!-- #main -->
